72d4a1d9f49f7d0eabd4c954cc0b8edf2b03fcbf
sachdevakamal <sachdeva423@gmail.com>
2026-02-28 18:21:40 -0500

    finished data on backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.get('/jokes', (req, res) => {
   res.json(jokes);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
